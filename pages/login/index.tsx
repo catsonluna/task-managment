19c07ef26e1856a6db12cefc2680d82ca6c3f635
Dragonbestgg1 <dragonbestgg1@gmail.com>
2023-11-13 18:44:06 +0200

    head

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -3,11 +3,12 @@ import { Inter } from 'next/font/google'
 import styles from '@/styles/login.module.css'
 import Header from '../components/header'
 import ReactModal from 'react-modal';
-import { useState } from 'react'; // Import useState hook
+import { useState } from 'react'; 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  const [isSignUp, setIsSignUp] = useState(false); // Add a state variable for signup
+  const [isSignUp, setIsSignUp] = useState(false); 
+  const [isModalOpen, setIsModalOpen] = useState(false); 
 
   return (
     <>
@@ -18,20 +19,27 @@ export default function Home() {
       </Head>
       <main className={`${styles.main}`}>
         <Header />
-        <div className={`${isSignUp ? styles.signup : styles.login}`}> {/* Use the state variable here */}
+        <div className={`${isSignUp ? styles.signup : styles.login}`}>
             <h2 className={styles.subTitle}>{isSignUp ? 'Signup' : 'Login'}</h2>
             <form action="#" className={`${styles.content}`}>
                 <input type="text" id="username" className={`${styles.input}`} placeholder='Username'/>
+                {isSignUp && <input type="email" id="email" className={`${styles.input}`} placeholder='Email'/>} 
                 <input type="password" id="password" className={`${styles.input}`} placeholder='Password'/>
-                <div className={`${styles.al}`}>
-                    <a>Forgot password?</a>
-                </div>
+                {!isSignUp && <div className={`${styles.al}`}> 
+                    <a onClick={() => setIsModalOpen(true)}>Forgot password?</a> 
+                </div>}
                 <button type="submit" className={`${styles.submit}`}>{isSignUp ? 'Signup' : 'Login'}</button>
             </form>
-            <a onClick={() => setIsSignUp(!isSignUp)}> {/* Add an onClick handler to toggle the state */}
+            <a onClick={() => setIsSignUp(!isSignUp)}> 
               {isSignUp ? 'Already have an account? Login' : 'Dont have an account? Signup'}
             </a>
         </div>
+        <ReactModal isOpen={isModalOpen} className={`${styles.mod}`}> 
+          <h2>Reset Password</h2>
+          <input type="email" id="email" className={`${styles.inputmod}`} placeholder='Email'/>
+          <button type="submit" className={`${styles.submitmod}`}>Submit</button> 
+          <button onClick={() => setIsModalOpen(false)} className={`${styles.submitmod}`}>Close</button> 
+        </ReactModal>
       </main>
     </>
   )
