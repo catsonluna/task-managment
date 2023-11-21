502b0f9071eeb63d8dfaf22c91ae3416ed6a16b4
pinkulu <56201697+pinkulu@users.noreply.github.com>
2023-11-21 09:07:56 +0200

    Uzdevuma apraksts:
    Jūsu uzdevums ir izstrādāt vienkāršu darba plānošanas tīmekļa vietni, kas palīdzēs lietotājiem organizēt savas ikdienas uzdevumus un darbu. Tīmekļa vietnei jābūt interaktīvai un ērti lietojamai, lai lietotāji varētu efektīvi plānot un pārvaldīt savu darbu.
    
    Sākotnēji izstrādā tīmekļa vietnes pamata dizainu un lietotāja saskarni, kurā iekļauj šādas funkcionalitātes:
    a. Lietotāja reģistrācija un pieteikšanās sistēma.
    
    b. Darba uzdevumu pievienošana, kur katram uzdevumam ir šādas informācijas vienības:
    Nosaukums
    Apraksts
    Termiņš
    
    c. Uzdevumu saraksta attēlošana, kur lietotāji var apskatīt savus pievienotos uzdevumus.
    
    d. Uzdevumu atzīmēšana kā pabeigtus vai nepabeigtus.
    
    e. Uzdevumu dzēšana.
    
    f. Darba plānošanas kalendārs, kur var apskatīt visus uzdevumu termiņus.
    
    g. Meklēšanas funkcija, lai ātri atrastu konkrētu uzdevumu.
    
    Izveido datu bāzi, kurā tiek glabāti lietotāju reģistrācijas dati un uzdevumu informācija. Lietotājiem jāvar piekļūt savam personīgajam kontam un datiem, un jābūt iespējai atgūt paroli, ja to aizmirsuši.
    
    Nodrošini, lai tīmekļa vietne būtu responsīva un pielāgojas dažādām ierīcēm un ekrāna izmēriem.
    
    Papildini tīmekļa vietni ar kādu papildu funkcionalitāti, piemēram, iespēju izveidot projektus vai kategorijas uzdevumiem, sadalīt uzdevumus atbilstoši to prioritātei vai sarežģītībai, un iestatīt atgādinājumus termiņu tuvošanās gadījumā.
    
    Vizuāli piemēri:

--- pages/api/auth/login.ts
+++ pages/api/auth/login.ts
@@ -2,7 +2,8 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import type { SessionToken } from '@/pages/api/types'
 import { connection } from '@/lib/database';
-import { checkEmail } from '@/lib/utils';
+import { checkEmail, shaHash } from '@/lib/utils';
+import { verifyPassword, generateSecret } from '@/lib/utils';
 export default function handler(
     req: NextApiRequest,
     res: NextApiResponse<SessionToken>
@@ -15,6 +16,81 @@ export default function handler(
             },
         });
     }
+
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+        return res.status(400).json({
+            success: {
+                success: false,
+                cause: "missing_parameters"
+            },
+        });
+    }
+
+    connection.query(
+        `SELECT * FROM users WHERE username = ? or email = ?`,
+        [username, username],
+        (error, results, fields) => {
+            if (error) {
+                return res.status(500).json({
+                    success: {
+                        success: false,
+                        cause: "internal_server_error"
+                    },
+                });
+            }
+
+            if (results.length === 0) {
+                return res.status(401).json({
+                    success: {
+                        success: false,
+                        cause: "invalid_credentials"
+                    },
+                });
+            }
+
+            const user = results[0];
+
+            if (!verifyPassword(password, user.password)) {
+                return res.status(401).json({
+                    success: {
+                        success: false,
+                        cause: "invalid_credentials"
+                    },
+                });
+            }
+
+            const session_id = generateSecret(32);
+            const session_token = generateSecret(32);
+            const session_hash = shaHash(session_token);
+            
+            connection.query(
+                `INSERT INTO sessions (user_id, session_id, session_hash) VALUES (?, ?, ?)`,
+                [user.id, session_id, session_hash],
+                (error, results, fields) => {
+                    if (error) {
+                        return res.status(500).json({
+                            success: {
+                                success: false,
+                                cause: "internal_server_error"
+                            },
+                        });
+                    }
+
+                    return res.status(200).json({
+                        success: {
+                            success: true,
+                        },
+                        data: {
+                            token: session_token,
+                        }
+                    });
+                }
+            );
+        }
+    );
+
 
     
 }
