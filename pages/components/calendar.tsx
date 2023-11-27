5d82acff26b235c2c408b59c153c1c8f628f171d
Dragonbestgg1 <dragonbestgg1@gmail.com>
2023-11-27 15:25:13 +0200

    new cal

--- pages/components/calendar.tsx
+++ pages/components/calendar.tsx
@@ -1,11 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import dynamic from 'next/dynamic';
+import React, { useState } from 'react';
 import styles from '@/styles/cal.module.css';
+import DateTime from 'react-datetime'; 
+import 'react-datetime/css/react-datetime.css'; 
 
 function Cal() {
+  const [dateTime, setDateTime] = useState(new Date());
+
   return (
-    <div className={`${styles.main}`}>
-      <input type="datetime-local" className={`${styles.cal}`} name="" id="" ></input>
+    <div className={styles.main}>
+      <div className={styles.calWrapper}>
+        <DateTime 
+          value={dateTime} 
+          onChange={setDateTime} 
+          inputProps={{ 
+            className: styles.cal, 
+            readOnly: true 
+          }} 
+        />
+      </div>
     </div>
   );
 }
