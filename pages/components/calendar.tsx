d06d846b8f18ebfcee50374a778a97ea3ea8830d
pinkulu <56201697+pinkulu@users.noreply.github.com>
2023-11-29 11:22:38 +0200

    fix

--- pages/components/calendar.tsx
+++ pages/components/calendar.tsx
@@ -2,16 +2,22 @@ import React, { useState } from 'react';
 import styles from '@/styles/cal.module.css';
 import DateTime from 'react-datetime'; 
 import 'react-datetime/css/react-datetime.css'; 
-
+import Moment from 'moment';
 function Cal() {
-  const [dateTime, setDateTime] = useState(new Date());
+  const [dateTime, setDateTime] = useState(Moment().add(30, "minutes"));
+
+
+
+  
 
   return (
     <div className={styles.main}>
       <div className={styles.calWrapper}>
         <DateTime 
           value={dateTime} 
-          onChange={setDateTime} 
+          onChange={(e) => {
+            setDateTime(e as Moment.Moment);
+          }} 
           inputProps={{ 
             className: styles.cal, 
             readOnly: true 
